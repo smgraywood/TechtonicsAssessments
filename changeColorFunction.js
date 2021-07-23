5db8783fdd1af30f29ca793697601ac7ff76ef5a
Sarah Graywood <81881720+smgraywood@users.noreply.github.com>
2021-07-23 14:11:04 -0700

    Initial Commit

---
+++ changeColorFunction.js
@@ -0,0 +1,9 @@
+function changeBackgroundColor(){
+    let color = document.getElementById('colorInputText').value;
+    document.body.style.backgroundColor = color;
+  }
+  
+  function changeFontColor(){
+    let color = document.getElementById('colorInputFont').value;
+    document.body.style.color = color;
+  }
