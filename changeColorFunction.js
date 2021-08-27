1478c425a41e164bd2cdf62bdcbc45ea7bd94867
Sarah Graywood <81881720+smgraywood@users.noreply.github.com>
2021-08-27 14:33:29 -0700

    Delete changeColorFunction.js

--- changeColorFunction.js
+++
@@ -1,9 +0,0 @@
-function changeBackgroundColor(){
-    let color = document.getElementById('colorInputText').value;
-    document.body.style.backgroundColor = color;
-  }
-  
-  function changeFontColor(){
-    let color = document.getElementById('colorInputFont').value;
-    document.body.style.color = color;
-  }
