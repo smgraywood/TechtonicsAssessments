d79d48ea00d988d258677d9ef952883231a4d044
Sarah Graywood <smgraywood@gmail.com>
2021-08-27 14:32:29 -0700

    initial commit

---
+++ knowledge_check_week_7/AssessmentWeek7-main/src/App.js
@@ -0,0 +1,13 @@
+import './components/Form';
+import './App.css';
+import RegisterYourApplicationForm from './components/Form';
+
+function App() {
+  return (
+    <div className="App">
+      <RegisterYourApplicationForm />
+    </div>
+  );
+}
+
+export default App;
